4078f488d5bdb35a33dd09b3fa4bfa561ff1c791
Patrick Braun <prbraun@protonmail.com>
2021-06-09 22:59:57 -0500

    You can view employees by department

--- index.js
+++ index.js
@@ -122,10 +122,9 @@ async function viewEmployeesByDepartment() {
   const departments = await db.findAllDepartments();
 
   const departmentChoices = departments.map(({ id, name }) => ({
-    // CREATE TWO PROPERTIES name AMD value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE NAME OF THE DEPARTMENT.
-    // THE PROPERTY value SHOULD CONTAIN id.
     // THIS OBJECT FOR EACH MANAGER WILL RETURN TO MAP() TO CONSTRUCT AN ARRAY TO BE RETURNED AND BE STORED TO managerChoices.
-    // YOUR CODE HERE
+    name: name,
+    value: id
   }));
 
   const { departmentId } = await prompt([
@@ -149,10 +148,7 @@ async function updateEmployeeRole() {
   const employees = await db.findAllEmployees();
 
   const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
-    // CREATE TWO PROPERTIES name AMD value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE CONCATENATION OF THE FIRST HAME AND THE LAST NAME.
-    // THE PROPERTY value SHOULD CONTAIN id.
     // THIS OBJECT FOR EACH MANAGER WILL RETURN TO MAP() TO CONSTRUCT AN ARRAY TO BE RETURNED AND BE STORED TO managerChoices.
-    // YOUR CODE HERE 
     name: first_name.concat(last_name),
     value: id   
 
